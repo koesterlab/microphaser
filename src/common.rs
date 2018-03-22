e5d19c2f9cdcc703b7effafcd86af98f7400f2b1
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-03-22 12:15:44 +0100

    Use SOMATIC flag for determining is_germline.

--- src/common.rs
+++ src/common.rs
@@ -15,13 +15,7 @@ pub enum Variant {
 
 impl Variant {
     pub fn new(rec: &mut bcf::Record) -> Result<Vec<Self>, Box<Error>> {
-        let is_germline = any(rec.genotypes()?.get(1).iter().map(|gt_allele| {
-            match gt_allele {
-                &bcf::record::GenotypeAllele::Unphased(i) if i > 0 => true,
-                &bcf::record::GenotypeAllele::Phased(i) if i > 0 => true,
-                _ => false
-            }
-        }), |is_germline| is_germline);
+        let is_germline = !rec.info(b"SOMATIC").flag()?;
 
         let pos = rec.pos();
         let alleles = rec.alleles();
