964c3ae88c6bb139e872ea7558942eed21abb749
jafors <forster_jan@web.de>
2019-03-28 16:10:13 +0000

    fixed testing error and somatic.cli

--- tests/lib.rs
+++ tests/lib.rs
@@ -130,7 +130,7 @@ fn splice_test_forward_germline() {
     microphaser_normal(&format!("tests/resources/splice_forward_test/INSIG1.test.bam \
         --variants tests/resources/splice_forward_test/INSIG1.test.germline.vcf \
         --ref {} > tests/output/splice_forward_test.germline.fa < tests/resources/splice_forward_test/INSIG1.test.gtf", reference));
-    test_output("tests/output/splice_forward_test.germline.fa", "tests/resources/splice_forward_test/expected_output/splice_forward_test.fa");
+    test_output("tests/output/splice_forward_test.germline.fa", "tests/resources/splice_forward_test/expected_output/splice_forward_test.germline.fa");
 }
 
 #[test]
