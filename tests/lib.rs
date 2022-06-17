2288d36264e58d7e324ebab5de52fdcc35df50d1
David Laehnemann <david.laehnemann@hhu.de>
2022-06-17 13:33:37 +0200

    correct use statement for `hyper::client::Client`
    Apparently, public docs of `hyper` are not necessarily up to date, see for example these mentions of using `hyper::Client`:
    * https://hyper.rs/guides/client/basic/
    * https://docs.rs/hyper/0.14.19/hyper/client/index.html#example
    
    Instead, probably after a refactoring not reflected in the docs, one should follow the pattern `use hyper::client::Client` given here:
    https://docs.rs/hyper/latest/hyper/#reexports

--- tests/lib.rs
+++ tests/lib.rs
@@ -8,7 +8,7 @@ use std::process::Command;
 // use std::process;
 use std::io;
 
-use hyper::Client;
+use hyper::client::Client;
 
 fn test_output(result: &str, expected: &str) {
     assert!(Command::new("diff")
